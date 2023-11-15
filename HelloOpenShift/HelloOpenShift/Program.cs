05e3f7a17b9c681cdcc39fbb51274b6179ecabcd
Zero Amps <jozef.chovan@gmail.com>
2023-11-15 09:53:01 +0100

    Create a .NET project for testing

---
+++ HelloOpenShift/HelloOpenShift/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloOpenShift;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
+
